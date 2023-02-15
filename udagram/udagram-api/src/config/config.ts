d2e06590b37b14f68d71583d7e190a8d8d6f5075
gasser <gasserkhaled23@gmail.com>
2023-02-15 22:13:13 +0200

    update sequelize.ts

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -1,4 +1,5 @@
 import * as dotenv from "dotenv";
+import * as process from "process";
 dotenv.config();
 
 // ENV variables 
@@ -10,10 +11,12 @@ export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
+  port: Number(process.env.PORT),
   host: process.env.POSTGRES_HOST,
-  aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
-  aws_media_bucket: process.env.AWS_BUCKET,
+  dialect: "postgres",
+  awsRegion: process.env.AWS_REGION,
+  awsProfile: process.env.AWS_PROFILE,
+  awsMediaBucket: process.env.AWS_BUCKET,
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
